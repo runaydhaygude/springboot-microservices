f6db922f12ee01a4b8517ab532ce81cc6624ee61
Runay Dhaygude <runaydhaygude@Runays-MacBook-Air.local>
2023-09-13 13:27:48 +0530

    sevices to create the tours

---
+++ src/main/java/com/example/microservices/service/TourPackageService.java
@@ -0,0 +1,43 @@
+package com.example.microservices.service;
+
+import com.example.microservices.domain.TourPackage;
+import com.example.microservices.repo.TourPackageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class TourPackageService {
+    private final TourPackageRepository tourPackageRepository;
+
+    @Autowired
+    public TourPackageService(TourPackageRepository tourPackageRepository) {
+        this.tourPackageRepository = tourPackageRepository;
+    }
+
+    /**
+     * Create a Tour Package
+     *
+     * @param code code of the package
+     * @param name name of the package
+     *
+     * @return new or existing tour package
+     */
+    public TourPackage createTourPackage(String code, String name) {
+        return tourPackageRepository.findById(code).orElse(tourPackageRepository.save(new TourPackage(code, name)));
+    }
+
+    /**
+     * Lookup All Tour packages
+     *
+     * @return
+     */
+    public Iterable<TourPackage> lookup(){
+        return tourPackageRepository.findAll();
+    }
+
+    public long total() {
+        return tourPackageRepository.count();
+    }
+}
+
